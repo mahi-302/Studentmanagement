66ed8f29cee3014ed5b62c6024968748412b1e23
RambabuNadagana <94271381+RambabuNadagana@users.noreply.github.com>
2022-02-22 18:12:56 +0530

    Update jenkinsfile.tf

--- jenkinsfile.tf
+++ jenkinsfile.tf
@@ -1,39 +1,59 @@
-pipeline{
-  agent {label 'main'}
-    stages{
-       stage('hosting application'){
-        steps{
-          sh "ls"
-          sh "aws rds create-db-instance --db-instance-identifier test-mysql-final2 --db-name detsdb --db-instance-class db.t2.micro --vpc-security-group-ids  sg-0bb5391635b3c304e --engine mysql --engine-version 5.7 --db-parameter-group-name default.mysql5.7 --publicly-accessible  --master-username admin --master-user-password Ramrebel56 --allocated-storage 10 --region us-east-2"
-          sleep(450)
-          script{
-              def cmd = "aws rds describe-db-instances --db-instance-identifier test-mysql-final2 --region us-east-2"
-              def output = sh(script: cmd,returnStdout: true)
-              jsonitem = readJSON text: output
-              println(jsonitem)
-           }
-           sh "sed -i.bak 's/endpoint/${jsonitem['DBInstances'][0]['Endpoint']['Address']}/g' userdata.txt"
-          script{
-              def cmd = "aws elbv2 create-load-balancer --name my-load-balancer-final2 --subnets subnet-09521af8c6cfe39fb subnet-0080182dff1d4159a --security-groups sg-0bb5391635b3c304e --region us-east-2 "
-              def output = sh(script: cmd,returnStdout: true)
-              jsonitem1 = readJSON text: output
-              println(jsonitem1)
-              sleep(100)
-            }
-          script{
-              def cmd = "aws elbv2 create-target-group --name my-target-final2 --protocol HTTP --port 80 --target-type instance --vpc-idvpc-0f607673eab7d2eb7 --region us-east-2"
-              def output = sh(script: cmd,returnStdout: true)
-              jsonitem2 = readJSON text: output
-              println(jsonitem2)
-              sleep(180)
-               }
-           sh "aws elbv2 create-listener --load-balancer-arn ${jsonitem1['LoadBalancers'][0]['LoadBalancerArn']} --protocol HTTP --port 80 --default-actions Type=forward,TargetGroupArn=${jsonitem2['TargetGroups'][0]['TargetGroupArn']} --region us-east-2"
-           sh "aws autoscaling create-launch-configuration --launch-configuration-name my-lc2-final1 --image-id ami-0629230e074c580f2 --instance-type t2.micro --security-groups sg-0bb5391635b3c304e --key-name jenkins --iam-instance-profile ram-s3-role --user-data file://userdata.txt --region us-east-2"
-           sh "aws autoscaling create-auto-scaling-group --auto-scaling-group-name my-asg3-final1 --launch-configuration-name my-lc2-final1 --max-size 2 --min-size 1 --desired-capacity 1 --target-group-arns ${jsonitem2['TargetGroups'][0]['TargetGroupArn']} --availability-zones us-east-2b --region us-east-2"
-           sh "aws autoscaling create-auto-scaling-group --auto-scaling-group-name my-asg4-final1 --launch-configuration-name my-lc2-final1 --max-size 2 --min-size 1 --desired-capacity 1 --target-group-arns ${jsonitem2['TargetGroups'][0]['TargetGroupArn']} --availability-zones us-east-2b --region us-east-2"
-           sh "aws autoscaling put-scaling-policy --auto-scaling-group-name my-asg3-final1 --policy-name alb1000-target-tracking-scaling-policy --policy-type TargetTrackingScaling --target-tracking-configuration file://config.json --region us-east-2"
-           sh "aws autoscaling put-scaling-policy --auto-scaling-group-name my-asg4-final1 --policy-name alb1000-target-tracking-scaling-policy --policy-type TargetTrackingScaling --target-tracking-configuration file://config.json --region us-east-2"
-        }
-       }
+provider "aws" {
+  region = "us-east-2"
+}
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  name                 = "cpms"
+  identifier           = "myrdb2"
+  username             = "admin"
+  password             = "Ramrebel56"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+  publicly_accessible  = true
+  vpc_security_group_ids = ["sg-0bb5391635b3c304e"]
+}
+resource "aws_launch_configuration" "as_conf" {
+  depends_on = [aws_db_instance.default]
+  name_prefix   = my-lc3-cli
+  image_id      = ami-0629230e074c580f2
+  instance_type = t2.micro
+  security_groups    = sg-0bb5391635b3c304es
+  key_name = "jenkins"
+  iam_instance_profile = "ram-s3-role"
+   user_data = templatefile("${path.module}/userdata.tftpl", {endpoint = aws_db_instance.default.endpoint,password = aws_db_instance.default.password,address = aws_db_instance.default.address})
+}
+resource "aws_autoscaling_group" "bar" {
+  name                 = my-asg3-cli 
+  depends_on           = ["aws_launch_configuration.as_conf"]
+  launch_configuration = aws_launch_configuration.as_conf.my-lc3-cli
+  min_size             = 1
+  max_size             = 2
+  desired_capacity     = 1
+  target_group_arns   = [aws_lb_target_group.test.us-east-2:931430496116:autoScalingGroup:7b57d37c-5d34-434a-ba6d-d0f7fd8ffa90:autoScalingGroupName/my-asg3-cli]
+ availability_zones = us-east-2b
+}
+resource "aws_lb" "test" {
+  name               = my-load-balancer
+  internal           = false
+  load_balancer_type = application
+  security_groups    = sg-0bb5391635b3c304e
+  subnets            = subnet-0080182dff1d4159a
+}
+resource "aws_lb_target_group" "test" {
+  name     = my-targets
+  port     = 80
+  protocol = HTTP
+  vpc_id   = vpc-0f607673eab7d2eb7
+}
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = arn:aws:elasticloadbalancing:us-east-2:931430496116:loadbalancer/app/my-load-balancer/70a9cbee293d24d9
+  port              = "80"
+  protocol          = HTTP
+  default_action {
+    type             = "Instance"
+    target_group_arn = arn:aws:elasticloadbalancing:us-east-2:931430496116:targetgroup/my-targets/26b72c4337de35a4
   }
 }
